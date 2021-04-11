70348beba5db26757893cf087a1eabc2463da5c7
Emotion <JDRCaple@outlook.com>
2021-04-11 15:35:21 +1200

    Updating Stuff

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -12,4 +12,4 @@ client.on('message', msg => {
     }
 });
 
-client.login(config.token);
+client.login(token);
